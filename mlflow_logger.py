c779a7c6a4989888e4274ce15fc54b957bc9b339
gigiDelMonaco <g.delmonaco1@studenti.unisa.it>
2022-07-20 09:39:03 +0200

    mflow logs on OneVsRest

--- mlflow_logger.py
+++ mlflow_logger.py
@@ -1,12 +1,12 @@
 import os
 import pandas as pd
-import sklearn
 from imblearn.over_sampling import SMOTE
 from collections import Counter
 from sklearn.model_selection import train_test_split, KFold, cross_validate
 from sklearn import preprocessing
 from sklearn import metrics
 from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import LinearSVC
 import mlflow
 import mlflow.sklearn
@@ -28,22 +28,30 @@ kf = KFold(n_splits=10)
 
 
 def eval_metrics(actual, pred):
-    precision = metrics.precision_score(actual, pred, average= 'weighted')
+    precision = metrics.precision_score(actual, pred, average='weighted')
     recall = metrics.recall_score(actual, pred, average='weighted')
-    f1_supp= metrics.f1_score(actual, pred, average='weighted')
+    f1_supp = metrics.f1_score(actual, pred, average='weighted')
     return precision, recall, f1_supp
 
 
-with mlflow.start_run():
-    for train_index, test_index in kf.split(X):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        X_train_scaled = std_scaler.fit_transform(X_train)
-        one_vs_rest_classifier = OneVsRestClassifier(LinearSVC(max_iter=15000))
-        one_vs_rest_classifier.fit(X_train_scaled, y_train)
-        y_predict = one_vs_rest_classifier.predict(X_test)
-        (precis, rec, f1) = eval_metrics(y_test, y_predict)
-        mlflow.log_param("OneVsRest", one_vs_rest_classifier)
-        mlflow.log_metric("precision", precis)
-        mlflow.log_metric("recall", rec)
-        mlflow.log_metric("f1", f1)
+models = [{"model_name": "KNN",
+           "instance": "KNeighborsClassifier(n_neighbors=7, metric='minkowski', p=2)",
+           "params": [{"k": "7"}, {"p": "2"}]
+           }]
+
+for model in models:
+    with mlflow.start_run():
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            X_train_scaled = std_scaler.fit_transform(X_train)
+            X_test_scaled = std_scaler.transform(X_test)
+            classifier = eval(model['instance'])
+            classifier.fit(X_train, y_train)
+            y_pred = classifier.predict(X_test)
+            (precis, rec, f1) = eval_metrics(y_test, y_pred)
+            for key, param in model['params']:
+                mlflow.log_param(key, param)
+            mlflow.log_metric("precision", precis)
+            mlflow.log_metric("recall", rec)
+            mlflow.log_metric("f1", f1)
